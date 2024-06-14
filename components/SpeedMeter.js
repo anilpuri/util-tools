0c1b416d2067238de99eed48ba933ae74281be2b
anilpuri <54407833+anilpuri@users.noreply.github.com>
2024-06-14 14:05:48 +0530

    Update SpeedMeter.js

--- components/SpeedMeter.js
+++ components/SpeedMeter.js
@@ -3,23 +3,26 @@ import { useEffect, useState } from "react";
 import calculateDistance from "@/utils/calculateDistance";
 import getGeoLocation from "@/utils/getGeoLocation";
 const SpeedMeter = () => {
-  const [currentLocation, setCurrentLocation] = useState();
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [tracking, setTracking] = useState(false);
   const [distanceCovered, setDistanceCovered] = useState(0);
   const [currentSpeed, setCurrentSpeed] = useState(0);
   //units =  "km" | "mile" | "meter" | "nmi"
-  const [selectedUnit, setSelectedUnit] = useState("meter");
+  const [selectedUnit, setSelectedUnit] = useState("km");
   useEffect(() => {
-    const interval = setInterval(() => {
-      intervalFunction();
-    }, 1000);
+    let interval;
+    if (tracking) {
+      interval = setInterval(() => {
+        intervalFunction();
+      }, 1000);
+    }
+
     return () => clearInterval(interval);
-  }, [distanceCovered, currentLocation, selectedUnit]);
+  }, [distanceCovered, currentLocation, selectedUnit, currentSpeed,tracking]);
 
   const intervalFunction = async () => {
     var currentLocationTemp = await getGeoLocation();
     if (currentLocationTemp) {
-      console.log(currentLocationTemp);
-      console.log(currentLocation);
       var distance = calculateDistance(
         currentLocation,
         currentLocationTemp,
@@ -34,92 +37,145 @@ const SpeedMeter = () => {
 
   const changeSelectedUnit = (newUnit) => {
     setDistanceCovered(0);
+    setCurrentSpeed(0);
     setSelectedUnit(newUnit);
   };
   return (
-    <div style={{ paddingTop: "200px" }}>
-      <h1 style={{ width: "100%", textAlign: "center" }}>
-        Current Speed : {currentSpeed}{" "}
-        {{ meter: "m/h", nmi: "nmi/h", mile: "mph", km: "km/h" }[selectedUnit]}
-      </h1>
-      <h2 style={{ width: "100%", textAlign: "center" }}>
-        Distance Covered : {distanceCovered}{" "}
-        {
-          { meter: "Meters", nmi: "Nmi's", mile: "Miles", km: "Km's" }[
-            selectedUnit
-          ]
-        }
-      </h2>
-      <div style={{ width: "100%", textAlign: "center", padding: "10px" }}>
-        <button
-          style={{
-            padding: "10px 15px",
-            fontSize:"16px",
-            margin: "5px",
-            cursor: "pointer",
-            border: "none",
-            backgroundColor: `${
-              selectedUnit === "meter" ? "#282727" : "rgb(107, 107, 107)"
-            }`,
-          }}
-          onClick={() => {
-            changeSelectedUnit("meter");
-          }}
-        >
-          Meter
-        </button>
-        <button
-          style={{
-            padding: "10px 15px",
-            fontSize:"16px",
-            margin: "5px",
-            cursor: "pointer",
-            border: "none",
-            backgroundColor: `${
-              selectedUnit === "mile" ? "#282727" : "rgb(107, 107, 107)"
-            }`,
-          }}
-          onClick={() => {
-            changeSelectedUnit("mile");
-          }}
-        >
-          Mile
-        </button>
-        <button
-          style={{
-            padding: "10px 15px",
-            fontSize:"16px",
-            margin: "5px",
-            cursor: "pointer",
-            border: "none",
-            backgroundColor: `${
-              selectedUnit === "km" ? "#282727" : "rgb(107, 107, 107)"
-            }`,
-          }}
-          onClick={() => {
-            changeSelectedUnit("km");
-          }}
-        >
-          Km
-        </button>
-        <button
-          style={{
-            padding: "10px 15px",
-            fontSize:"16px",
-            margin: "5px",
-            cursor: "pointer",
-            border: "none",
-            backgroundColor: `${
-              selectedUnit === "nmi" ? "#282727" : "rgb(107, 107, 107)"
-            }`,
-          }}
-          onClick={() => {
-            changeSelectedUnit("nmi");
-          }}
-        >
-          Nmi
-        </button>
-      </div>
+    <div>
+      {`Latitude: ${currentLocation?.latitude ?? 0}, Longitude: ${
+        currentLocation?.longitude ?? 0
+      }`}
+      {tracking ? (
+        <div style={{ paddingTop: "200px" }}>
+          <h1 style={{ width: "100%", textAlign: "center" }}>
+            Current Speed : {currentSpeed}{" "}
+            {
+              { meter: "m/h", nmi: "nmi/h", mile: "mph", km: "km/h" }[
+                selectedUnit
+              ]
+            }
+          </h1>
+          <h2 style={{ width: "100%", textAlign: "center" }}>
+            Distance Covered : {distanceCovered}{" "}
+            {
+              { meter: "Meters", nmi: "Nmi's", mile: "Miles", km: "Km's" }[
+                selectedUnit
+              ]
+            }
+          </h2>
+          <div style={{ width: "100%", textAlign: "center", padding: "10px" }}>
+            <button
+              style={{
+                padding: "10px 15px",
+                fontSize: "16px",
+                margin: "5px",
+                cursor: "pointer",
+                border: "none",
+                backgroundColor: `${
+                  selectedUnit === "meter" ? "#282727" : "rgb(107, 107, 107)"
+                }`,
+              }}
+              onClick={() => {
+                changeSelectedUnit("meter");
+              }}
+            >
+              Meter
+            </button>
+            <button
+              style={{
+                padding: "10px 15px",
+                fontSize: "16px",
+                margin: "5px",
+                cursor: "pointer",
+                border: "none",
+                backgroundColor: `${
+                  selectedUnit === "mile" ? "#282727" : "rgb(107, 107, 107)"
+                }`,
+              }}
+              onClick={() => {
+                changeSelectedUnit("mile");
+              }}
+            >
+              Mile
+            </button>
+            <button
+              style={{
+                padding: "10px 15px",
+                fontSize: "16px",
+                margin: "5px",
+                cursor: "pointer",
+                border: "none",
+                backgroundColor: `${
+                  selectedUnit === "km" ? "#282727" : "rgb(107, 107, 107)"
+                }`,
+              }}
+              onClick={() => {
+                changeSelectedUnit("km");
+              }}
+            >
+              Km
+            </button>
+            <button
+              style={{
+                padding: "10px 15px",
+                fontSize: "16px",
+                margin: "5px",
+                cursor: "pointer",
+                border: "none",
+                backgroundColor: `${
+                  selectedUnit === "nmi" ? "#282727" : "rgb(107, 107, 107)"
+                }`,
+              }}
+              onClick={() => {
+                changeSelectedUnit("nmi");
+              }}
+            >
+              Nmi
+            </button>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <button
+              style={{
+                padding: "10px 25px",
+                fontSize: "22px",
+                borderRadius: "8px",
+                margin: "5px",
+                cursor: "pointer",
+                border: "none",
+                backgroundColor: `${
+                  selectedUnit === "meter" ? "#282727" : "rgb(107, 107, 107)"
+                }`,
+              }}
+              onClick={() => {
+                setTracking(!tracking);
+              }}
+            >
+              Stop Tracking
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div style={{ paddingTop: "200px", textAlign: "center" }}>
+          <button
+            style={{
+              padding: "10px 25px",
+              fontSize: "22px",
+              borderRadius: "8px",
+              margin: "5px",
+              cursor: "pointer",
+              border: "none",
+              backgroundColor: `${
+                selectedUnit === "meter" ? "#282727" : "rgb(107, 107, 107)"
+              }`,
+            }}
+            onClick={() => {
+              setTracking(!tracking);
+            }}
+          >
+            Start Tracking
+          </button>
+        </div>
+      )}
     </div>
   );
 };
